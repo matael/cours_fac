0c87ed0e8a2852f31b35c6afeba1beb313b99cd0
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-04-04 19:20:25 +0200

    Script pour faire des listes de defs

---
+++ tools/mkdefsheet.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use 5.010;
+use IO::File;
+
+if (scalar(@ARGV) != 1) {
+    say "You should specify one (and only one) filename";
+    exit(0);
+}
+
+my $key = "!";
+my $value = "!";
+
+my $dest = IO::File->new($ARGV[0], "w") or die("$!");
+$dest->print('
+\documentclass[a4paper, 11pt]{article}
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage[french]{babel}
+
+\title{Insert Title here}
+\author{Who are you ?}
+\date{}
+
+\begin{document}
+\maketitle
+\begin{descritpion}
+');
+
+until ($key eq "" or $value eq ""){
+    print "Definen : ";
+    $key = <STDIN>;
+    print "Definiendum : ";
+	$value = <STDIN>;
+    chomp($key);
+    chomp($value);
+    $dest->print("\\item[$key] $value\n") unless ($key eq "" or $value eq "");
+}
+
+$dest->print('\end{description}
+\end{document}');
+$dest->close();
