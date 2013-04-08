657cd69321e93681973157994893e9ab79c8e984
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-04-08 08:40:30 +0200

    Maj de fichiers (old)

--- maths/l2_dm1/exo3.m
+++ maths/l2_dm1/exo3.m
@@ -2,7 +2,8 @@ clear all;
 close all;
 
 % on crée nos axes x et y
-tx = ty = linspace(0, 10, 51);
+tx = ty = linspace(-100, 100, 201);
 [xx, yy] = meshgrid(tx, ty);
 tz = -500 + (xx.^4).*(yy.^2) + log(1 + 4*xx.^2 + 5*yy.^2);
 mesh(tx, ty, tz);
+print("exo3.png", "-dPNG");
