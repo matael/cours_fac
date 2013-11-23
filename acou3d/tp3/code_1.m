aff70073e8da0618dea3200a2a7307e6716c5720
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-11-23 21:10:41 +0100

    Correction du drift temporel

--- acou3d/tp3/code_1.m
+++ acou3d/tp3/code_1.m
@@ -19,6 +19,19 @@ P2 = Re_c2 + i*Im_c2;
 P0 = ( P1 + P2 ) ./ (2*cos(k/2 *10^-2));
 V0 = (P1 - P2) ./ (i*2*rho*c*sin(k*10^-2/2));
 
+V0prev = rho*c*P0;
+% rectification du drift temporel
+[m, id_v0] = max(V0);
+[m, id_V0prev] = max(V0prev);
+drift = id_v0 -  id_V0prev
+if (drift > 0)
+	V0prev = [zeros(abs(drift),1) ; V0prev(1:end-abs(drift))];
+elseif (drift < 0)
+	V0prev = [V0prev(abs(drift)+1:end) ; zeros(abs(drift),1)];
+	length(V0prev)
+	length(F)
+end
+
 figure(1);
 plot(F,P0);
 grid on;
@@ -28,3 +41,13 @@ figure(2);
 plot(F,V0);
 grid on;
 title('Vitesse en 0');
+
+figure(3);
+plot(F,V0/max(V0));
+hold on;
+plot(F, V0prev/max(V0prev), 'r');
+grid on;
+legend("v_0", "Zp_0");
+title("Vitesses mesuree et previsionnelle en O (apres normalisation a 1)");
+xlabel("Frequence (Hz)");
+ylabel("Vitesse (normalisee)");
