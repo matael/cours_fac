3af32f83126f9939d3dadf9423bec1ffc280ed89
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-12-13 16:35:07 +0100

    Retour des directivités

---
+++ acou3d/tp1/direct_quadri.m
@@ -0,0 +1,39 @@
+%
+% direct_quadri.m
+%
+% Copyright (C) 2013 Mathieu Gaborit (matael) <mathieu@matael.org>
+%
+%
+% Distributed under WTFPL terms
+%
+%            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+%                    Version 2, December 2004
+%
+% Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+%
+% Everyone is permitted to copy and distribute verbatim or modified
+% copies of this license document, and changing it is allowed as long
+% as the name is changed.
+%
+%            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+%   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+%
+%  0. You just DO WHAT THE FUCK YOU WANT TO.
+%
+
+clear all;
+close all;
+
+% get meas data
+run 'mesures/directivite_quadripole.data';
+
+meas_data = normOnZero(meas_data);
+
+% gh = polar(meas_data(:,1), meas_data(:,2));
+% set(gh, 'LineWidth', 3)
+% polargrid;
+dirplot(meas_data(:,1)*180/pi , 10*log10(meas_data(:,2)));
+title("Directivite du quadripole");
+print('-dpng', 'direct_quadri.png');
+
+
